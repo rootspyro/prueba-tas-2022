f58f652b61d0263860ef62a95cd2a3c5052b551e
rootspyro <root.spyro@gmail.com>
2022-01-10 00:56:20 -0400

    footer modified and home presentation added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faStore } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  faStore = faStore;
 
 }
